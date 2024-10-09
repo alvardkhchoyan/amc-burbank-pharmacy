99746eb87645281db4dd3a9e237b561846a4326a
Lily <liliazatyan2000@gmail.com>
2024-10-09 19:59:16 +0400

    writting all text in project

--- src/pages/product/Product.tsx
+++ src/pages/product/Product.tsx
@@ -7,7 +7,9 @@ const ProductSection = () => {
       {/* First Section */}
       <div className="product-first-section">
         <div className="product-text text-color-1">
-          <h2 className="product-title">Premium Pharmacy Drugs for Your Health</h2>
+          <h2 className="product-title">
+            Premium Pharmacy Drugs for Your Health
+          </h2>
           <p className="description-for-product">
             At our pharmacy, we pride ourselves on offering only the highest
             quality drugs to ensure your health and well-being. Each product is
@@ -30,12 +32,17 @@ const ProductSection = () => {
           alt="Product 2"
         />
         <div className="product-text text-color-2">
-          <h2 className="product-title">Drug Name Here</h2>
+          <h2 className="product-title">Dr Lily Ros Natural Skin Care</h2>
           <p className="description-for-product">
-            This is a highly effective drug used for treating various ailments.
-            Known for its superior quality and quick relief, it is one of the
-            best choices available in the market. Trust our pharmacy to provide
-            you with top-notch medications to improve your health.
+            Dr. Lily Ros is a registered pharmacist and the founder of Dr. Lily
+            Ros Organics, a brand dedicated to crafting all-natural, plant-based
+            skincare, body care, and makeup products. Driven by her pharmacology
+            expertise and a passion for skincare, Dr. Ros formulates each
+            product in her laboratory with a focus on safety, effectiveness, and
+            luxury. Her journey into skincare began during her pregnancy when
+            she became more mindful of what she applied to her skin, leading her
+            to create a range of products using 100% natural and organic
+            botanicals.
           </p>
         </div>
       </div>
@@ -43,13 +50,19 @@ const ProductSection = () => {
       {/* Third Section */}
       <div className="product-third-section">
         <div className="product-text text-color-3">
-          <h2 className="product-title">Another Drug Name Here</h2>
+          <h2 className="product-title">Juzo Compression Products</h2>
           <p className="description-for-product">
-            Discover the power of our latest pharmaceutical innovations. This
-            drug is designed to tackle specific health issues with precision and
-            efficiency. Backed by research and trusted by healthcare
-            professionals, it promises to deliver outstanding results for
-            patients seeking quick recovery and long-term health.
+            At AMC Burbank Pharmacy, we proudly offer a selection of Juzo
+            products to meet your needs. Their garments are crafted with
+            advanced materials that provide effective compression while ensuring
+            breathability and ease of wear. Ideal for individuals that need
+            improvement of circulation and support those with various medical
+            conditions. Juzo products not only promote health and wellness but
+            also come in a variety of styles and colors, allowing users to
+            express their personal style. Juzo is a leading brand specializing
+            in high-quality compression garments Juzo offers a wide range of
+            products, including stockings, sleeves, and bandages, tailored for
+            both therapeutic and everyday use.
           </p>
         </div>
         <img
@@ -67,25 +80,24 @@ const ProductSection = () => {
           alt="Product 4"
         />
         <div className="product-text text-color-4">
-          <h2 className="product-title">Yet Another Drug Name</h2>
+          <h2 className="product-title">Medical Supply</h2>
           <p className="description-for-product">
-            Introducing our latest addition to the pharmacy, designed to offer
-            effective relief for common health concerns. This product stands out
-            due to its advanced formulation and minimal side effects. Experience
-            the benefits of cutting-edge pharmaceutical research with every
-            dose.
+            At AMC Burbank Pharmacy, we understand that having the right medical
+            supplies is essential for managing your health and well-being.
+            That's why we offer a comprehensive selection of high-quality
+            medical products to meet your needs.
           </p>
         </div>
       </div>
       <div className="product-fifth-section">
         <div className="product-text text-color-3">
-          <h2 className="product-title">Another Innovative Drug Name</h2>
+          <h2 className="product-title">DR T Anti-Aging</h2>
           <p className="description-for-product">
-            Our newest pharmaceutical product is engineered to provide rapid and
-            effective treatment for a wide range of conditions. It leverages
-            state-of-the-art technology and scientific research, ensuring
-            maximum efficacy with minimal side effects. Trust in our dedication
-            to quality and patient care.
+            Dr. Tihana Skaricic, Doctor of Pharmacy & Integrative/Functional
+            Medicine Practitioner, is here to guide you on your journey to
+            optimal health at AMC Burbank Pharmacy. We proudly offer specialized
+            tests through Dr. T Anti-Aging, including innovative health
+            assessments tailored to your unique needs.
           </p>
         </div>
         <img
@@ -101,13 +113,14 @@ const ProductSection = () => {
           alt="Product 6"
         />
         <div className="product-text text-color-4">
-          <h2 className="product-title">Final Drug Name Example</h2>
+          <h2 className="product-title">Over The Counter Products</h2>
           <p className="description-for-product">
-            Experience the latest advancements in pharmaceutical care with our
-            new product. It offers a unique combination of ingredients to
-            support overall health and well-being. This product is crafted to
-            ensure high effectiveness and safety, providing an ideal solution
-            for maintaining good health and preventing illness.
+            Discover Your Health Solutions at AMC Burbank Pharmacy Your One-Stop
+            Shop for Over-the-Counter Medications! At AMC Burbank Pharmacy, we
+            offer a wide range of over-the-counter (OTC) products designed to
+            help you feel your best—without a prescription! Whether you're
+            dealing with a minor ailment or just stocking up for home care,
+            we've got you covered.
           </p>
         </div>
       </div>
